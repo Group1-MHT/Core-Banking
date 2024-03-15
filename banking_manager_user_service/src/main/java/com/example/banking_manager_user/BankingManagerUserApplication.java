565f16eff43fcccbe628c34bf6e834b56f75b488
belemon81 <2001040208@s.hanu.edu.vn>
2024-03-15 09:30:29 +0700

    Save changes

--- banking_manager_user_service/src/main/java/com/example/banking_manager_user/BankingManagerUserApplication.java
+++ banking_manager_user_service/src/main/java/com/example/banking_manager_user/BankingManagerUserApplication.java
@@ -2,6 +2,7 @@ package com.example.banking_manager_user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class BankingManagerUserApplication {
